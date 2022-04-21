96cc8f810ed085bbb5aad3b7fb2cb993a4d50f58
Virtus-web <nicolas.pedenau@hotmail.fr>
2022-04-21 14:30:00 +0200

    player not done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { ThemeContext } from './context'
+import { useState } from 'react'
 import VideoUpload from './components/VideoUpload'
 import VideoList from './components/VideoList'
+// import Player from './components/Player'
 import Header from './components/Header'
 
 function App() {
+
+    const [modal, setModal] = useState('')
+    const activeModal = () => {
+        setModal(modal === 'active' ? '' : 'active')
+    }
+
     return (
         <div className='container'>
+        <ThemeContext.Provider value={{ modal, activeModal }}>
             <Header />
             <VideoUpload />
             <VideoList />
+            {/* <Player /> */}
+        </ThemeContext.Provider>
         </div>
     )
 }
